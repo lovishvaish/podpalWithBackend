4720ff2ff5d49f88ad061707ade5388c8af59356
Lovish Vaish <lovisv@microsoft.com>
2022-09-22 10:59:55 -0700

    pull is working

--- ClientApp/src/components/ReportWhales.js
+++ ClientApp/src/components/ReportWhales.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 
 export class ReportWhales extends Component {
-  //static displayName = Home.name;
+
+  reportWhales() {
+    let latitude = document.getElementById("latitude").value;
+    let longitude = document.getElementById("longitude").value;
+    let jsonbody = {
+      "id": 5,
+      "longitude": parseInt(longitude),
+      "latitude":parseInt(latitude)
+  }
+  console.log(JSON.stringify(jsonbody));
+    // Send data to the backend via POST
+    fetch('https://localhost:7146/api/LocationsData', {  // Enter your IP address here
+
+      headers: {'Accept': 'application/json','Content-Type': 'application/json'},
+      method: 'POST', 
+      mode: 'no-cors', 
+      body: JSON.stringify(jsonbody) 
+    })   
+  }
 
   render() {
     return (
       <div>
         <h1>Report whales!</h1>
         <br></br><br></br>
-        <form action="/action_page.php">
-            <label for="latitude">Latitude:&nbsp;&nbsp;&nbsp;&nbsp;</label>
-            <input type="text" id="latitude" name="latitude"></input> <br></br><br></br>
-            <label for="longitude">Longitude:&nbsp;</label>
-            <input type="text" id="longitude" name="longitude"></input><br></br><br></br>
-            <input type="submit" value="Submit"></input>
-        </form>        
-      </div>      
+        <label for="latitude">Latitude:&nbsp;&nbsp;&nbsp;&nbsp;</label>
+        <input type="text" id="latitude" name="latitude"></input> <br></br><br></br>
+        <label for="longitude">Longitude:&nbsp;</label>
+        <input type="text" id="longitude" name="longitude"></input><br></br><br></br>
+        <button className="btn btn-primary" onClick={this.reportWhales}>Report</button>
+      </div>
     );
   }
 }
--- ClientApp/src/components/WhalesNearMe.js
+++ ClientApp/src/components/WhalesNearMe.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
+
+
 export class WhalesNearMe extends Component {
+
+  getData() 
+  { 
+   fetch('https://localhost:7146/api/LocationsData', {  // Enter your IP address here
+
+    headers: {'Content-Type': 'application/json'},
+    method: 'GET', 
+    mode: 'no-cors'
+  }).then((response) => {
+    console.log("promis",response.json());
+  });
+
+
+  }
+
   render() {
     return (
       <div>
         <h1>Whales near me!</h1>
+        <button className="btn btn-primary" onClick={this.getData}>Get Data</button>
       </div>
     );
   }
