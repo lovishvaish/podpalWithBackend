055580508cc9a1f1c8d06b5e644dd036559f3ef5
Lovish Vaish <lovisv@microsoft.com>
2022-09-22 08:39:36 -0700

    Update pages

--- ClientApp/src/components/ReportWhales.js
+++ ClientApp/src/components/ReportWhales.js
@@ -7,7 +7,15 @@ export class ReportWhales extends Component {
     return (
       <div>
         <h1>Report whales!</h1>
-      </div>
+        <br></br><br></br>
+        <form action="/action_page.php">
+            <label for="latitude">Latitude:&nbsp;&nbsp;&nbsp;&nbsp;</label>
+            <input type="text" id="latitude" name="latitude"></input> <br></br><br></br>
+            <label for="longitude">Longitude:&nbsp;</label>
+            <input type="text" id="longitude" name="longitude"></input><br></br><br></br>
+            <input type="submit" value="Submit"></input>
+        </form>        
+      </div>      
     );
   }
 }
