5f093fb1fc20890d30ecc3e8b39cacf5c506fa2c
Lovish Vaish <lovisv@microsoft.com>
2022-09-22 14:41:32 -0700

    Integratemaps

--- ClientApp/src/components/WhalesNearMe.js
+++ ClientApp/src/components/WhalesNearMe.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
+import BingMapsReact from "bingmaps-react";
 
 
 
 export class WhalesNearMe extends Component {
 
-   getData() {
-    let rep =fetch('https://localhost:7146/api/LocationsData', {  // Enter your IP address here
-      headers: { 'Content-Type': 'application/json' },
+
+
+  async getData() {
+    let rep = await fetch('https://localhost:7146/api/LocationsData', {  // Enter your IP address here
+      // headers: { 'Content-Type': 'application/json' },
       method: 'GET',
       mode: 'no-cors'
-    }).then((response) => response.json())
-    .then((data) => console.log(data));   
+    });
+    //let rep = await fetch('https://localhost:7146/api/LocationsData');
+    // let rep = await fetch('https://localhost:7146/api/LocationsData', {  // Enter your IP address here
+    //   // headers: { 'Content-Type': 'application/json' },
+    //   method: 'GET',
+    //   //   mode: 'no-cors'
+    // });
+    console.log(rep);
+    const data = await rep.text();
+    console.log(data);
+
   }
 
   render() {
     return (
       <div>
+        {/* <script src="http://dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0" type="text/javascript">          
+        </script> */}
         <h1>Whales near me!</h1>
         <button className="btn btn-primary" onClick={this.getData}>Get Data</button>
+        <BingMapsReact
+      bingMapsKey="Amhldcz34o9Q9k3vkKXOIYjgCXLxYJBf5asYmNXL6up5tPOFKmWuRQI9ZUppfoSU"
+      height="1000px"
+      mapOptions={{
+        navigationBarMode: "square",
+      }}
+      width="1000px"
+      viewOptions={{
+        center: { latitude: 42.360081, longitude: -71.058884 },
+        mapTypeId: "grayscale",
+      }}
+    />        
+        {/* <body>
+          <div id="divMap" style="position:relative;width:400px;   height:400px;border:1px solid blue;"></div>
+        </body> */}
       </div>
     );
   }
